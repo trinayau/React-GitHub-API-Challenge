df07d29bde0982ceae65a6058d4132aa8e0f74f3
“scralfie” <kellyalfie6@gmail.com>
2022-05-11 11:15:36 +0100

    added page and component folders

--- src/store.js
+++ src/store.js
@@ -2,8 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { searchReducer } from './reducers';
+import { Reducer } from './reducers';
 
-const store = createStore(searchReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(Reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
