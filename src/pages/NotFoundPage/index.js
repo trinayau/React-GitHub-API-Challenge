26735a90e527a2b9acc5a05e90207bd7d7f5a88a
Trina <92634994+trinayau@users.noreply.github.com>
2022-05-13 09:47:49 +0100

    added 404 page

--- src/pages/NotFoundPage/index.js
+++ src/pages/NotFoundPage/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import './styles.css';
 import * as components from '../../components';
-
+import darth from './darth.jpg'
 
 const NotFoundPage = () => {
 
-    return <>
+    return <div className="notfound-container">
 
 
-    <div id="404">
-        <img src="./darth.jpg" alt="Darth Vader" />
+    <div className="darth">
+        <h1>404 Page not found!</h1>
+        <p>I am not your father, please go back home.</p>
+        {/* <img src={darth} alt="Darth Vader" /> */}
     </div>
     
-    </>
+    </div>
 }
 
 export default NotFoundPage;
